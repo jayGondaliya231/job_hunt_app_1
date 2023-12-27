f2faf5f66a50a39f9b1728758244b0122c1a9a90
jaygo <jaygondaliya231@gmail.com>
2023-12-27 21:47:31 +0530

    Initial comment
    This is just the plain flutter starting project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:job_hunt_app/screen/Register_Screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Register_screen(),
+    );
+  }
+}
+
