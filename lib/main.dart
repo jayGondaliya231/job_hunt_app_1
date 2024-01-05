84ca3064bba1c71c8fbc91ae74d1e30221492aea
VICTUS <sagarsahil3690@gmail.com>
2024-01-05 22:11:57 +0530

    SAHIL

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home:Register_screen(),
     );
   }
