d39a88bcb321e6fea220fa73a63143e283aff5a0
jaygo <jaygondaliya231@gmail.com>
2024-02-04 20:44:31 +0530

    jay

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:job_hunt_app/view/auth/Login_Screen.dart';
 import 'package:job_hunt_app/view/auth/Register_Screen.dart';
-import 'package:job_hunt_app/view/auth/Splash_Screen.dart';
-import 'package:job_hunt_app/view/auth/Welcome_Screen.dart';
-import 'package:job_hunt_app/view/auth/selection_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    color: Colors.white, 
+      color: Colors.white,
       debugShowCheckedModeBanner: false,
       home: Register_screen(),
     );
