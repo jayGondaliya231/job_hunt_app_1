c269c88a209e7865ce072d851d4753c23df3b78a
VICTUS <sagarsahil3690@gmail.com>
2024-02-21 22:41:58 +0530

    sahil

---
+++ lib/view/auth/drawer_.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Drawerpage extends StatefulWidget {
+  const Drawerpage({super.key});
+
+  @override
+  State<Drawerpage> createState() => _DrawerpageState();
+}
+
+class _DrawerpageState extends State<Drawerpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize{
+        PreferredSize:Size.fromHeight(50,h)
+    },
+    );
+  }
+}
